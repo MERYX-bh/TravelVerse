c90fd53bc9a13e64762e21ef551ea89cd1f66085
MERYX-bh <jm_baha@esi.dz>
2023-08-25 07:55:15 +0200

    full project'

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -13,8 +13,44 @@ const Footer = () => (
   >
     <div className="footer-gradient"/>
     <div className={`${styles.innerWidth} mx-auto flex
-    flex-col gap-8`}>
+    flex-col gap-8 `}>
+      <div className="flex items-center justify-between
+      flex-wrap gap-5">
+        <h4 className="font-bold md:text-[64px]
+        text-[44px] text-white">
+          Enter the metaverse
+        </h4>
+        <button type="button" className="flex items-center
+        h-fit py-4 px-6 bg-[#25618b] rounded-[32px] gap-[12px]">
+          <img
+          src="/headset.svg"
+          className="w-[24px] h-[24px] object-contain"
+          />
+          <span className="font-normal text-[16px] text-white">ENTER METAVERSE</span>
+        </button>
+      </div>
 
+      <div className="flex flex-col">
+        <div className="mb-[50px] h-[2px] opacity-10 bg-white" />
+        <div className="flex items-center justify-between flex-wrap gap-4">
+          <h4 className="font-extrabold text-[24px]
+           text-white">Metaversus</h4>
+          <p className="font-normal text-[14px] text-white opacity-50">
+            Copyright © 2021 - 2022 Metaversus. All rights reserved.
+          </p>
+          <div className="flex gap-4">
+            {socials.map((social) => (
+              <img
+              src={social.url}
+              key={social.name}
+              alt={social.name}
+              className="w-[24px] h-[24px] object-contain
+              cursor-pointer"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   </motion.footer>
 );
